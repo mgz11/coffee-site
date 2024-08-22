bfef332df8401fa22054e04f291906c8f35cd1f8
mgz11 <mgonz112000@gmail.com>
2024-08-22 16:00:54 -0700

    implemented adding, activating / deactivating, and deleting products

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import db from "@/db/db";
+import db from "../../../db/db";
 import { z } from "zod";
 import fs from "fs/promises";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 const fileSchema = z.instanceof(File, { message: "Required" });
 const imageSchema = fileSchema.refine(
@@ -18,7 +18,7 @@ const addSchema = z.object({
 	image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
 	const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
 	if (result.success === false) {
 		return result.error.formErrors.fieldErrors;
@@ -35,14 +35,39 @@ export async function addProduct(formData: FormData) {
 		Buffer.from(await data.image.arrayBuffer())
 	);
 
-	db.product.create({
-		data: {
-			name: data.name,
-			description: data.description,
-			priceInCents: data.priceInCents,
-			imageUrl: imagePath,
-		},
-	});
+	try {
+		const createdProduct = await db.product.create({
+			data: {
+				isAvailableForPurchase: false,
+				name: data.name,
+				description: data.description,
+				priceInCents: data.priceInCents,
+				imageUrl: imagePath,
+			},
+		});
+
+		// If the creation is successful, you can access the created object here
+		console.log("Product created successfully:", createdProduct);
+	} catch (err) {
+		console.error(`Error creating product:`, err);
+	}
 
 	redirect("/admin/products");
+}
+
+export async function toggleProductAvailability(
+	id: string,
+	isAvailableForPurchase: boolean
+) {
+	await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
+}
+
+export async function deleteProduct(id: string) {
+	const product = await db.product.delete({ where: { id } });
+	if (product === null) {
+		return notFound();
+	}
+
+	// Delete image if product is deleted
+	await fs.unlink(`public${product.imageUrl}`);
 }
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/pageHeader";
 import Link from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import {
+	ActiveToggleDropdownItem,
+	DeleteDropdownItem,
+} from "./_components/ProductActions";
 
 export default function AdminProductsPage() {
 	return (
@@ -17,7 +38,39 @@ export default function AdminProductsPage() {
 	);
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+	// Get the individual products
+	const products = await db.product.findMany({
+		select: {
+			id: true,
+			name: true,
+			priceInCents: true,
+			isAvailableForPurchase: true,
+		},
+		orderBy: { name: "asc" },
+	});
+
+	// Get the quantities of each product ordered
+	const productOrderQuantities = await db.orderItem.groupBy({
+		by: ["productId"],
+		_sum: {
+			quantity: true,
+		},
+	});
+
+	// Combine the individual product info with the quanity of each order
+	const productsWithQuantities = products.map((product) => {
+		const orderQuantity = productOrderQuantities.find(
+			(item) => item.productId === product.id
+		);
+		return {
+			...product,
+			totalQuantityOrdered: orderQuantity?._sum.quantity || 0, // Default to 0 if no orders
+		};
+	});
+
+	if (products.length === 0) return <p>No products found!</p>;
+
 	return (
 		<Table>
 			<TableHeader>
@@ -33,6 +86,52 @@ function ProductsTable() {
 					</TableHead>
 				</TableRow>
 			</TableHeader>
+			<TableBody>
+				{productsWithQuantities.map((product) => (
+					<TableRow key={product.id}>
+						<TableCell>
+							{product.isAvailableForPurchase ? (
+								<>
+									<CheckCircle2 className="stroke-lime-600" />
+									<span className="sr-only">Available</span>
+								</>
+							) : (
+								<>
+									<XCircle className="stroke-destructive" />
+									<span className="sr-only">Unavailable</span>
+								</>
+							)}
+						</TableCell>
+						<TableCell>{product.name}</TableCell>
+						<TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+						<TableCell>{formatNumber(product.totalQuantityOrdered)}</TableCell>
+						<TableCell>
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<MoreVertical />
+									<span className="sr-only">Actions</span>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent>
+									<DropdownMenuItem asChild>
+										<Link href={`/admin/products/${product.id}/edit`}>
+											Edit
+										</Link>
+									</DropdownMenuItem>
+									<ActiveToggleDropdownItem
+										id={product.id}
+										isAvailableForPurchase={product.isAvailableForPurchase}
+									/>
+									<DropdownMenuSeparator />
+									<DeleteDropdownItem
+										id={product.id}
+										disabled={product.totalQuantityOrdered > 0}
+									/>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
 		</Table>
 	);
 }
