f5ef14f767f2163fa4c8dadee99c90710b247bf8
mgz11 <mgonz112000@gmail.com>
2024-08-28 13:19:15 -0700

    implemented edit product page for admins

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -55,6 +55,57 @@ export async function addProduct(prevState: unknown, formData: FormData) {
 	redirect("/admin/products");
 }
 
+const editSchema = addSchema.extend({
+	image: imageSchema.optional(),
+});
+
+export async function updateProduct(
+	id: string,
+	prevState: unknown,
+	formData: FormData
+) {
+	const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
+	if (result.success === false) {
+		return result.error.formErrors.fieldErrors;
+	}
+
+	const data = result.data;
+	const product = await db.product.findUnique({ where: { id } });
+
+	if (product === null) return notFound();
+
+	// Defaults the image to whatever was there. If a new image is uploaded,
+	// the current image will be unlinked and replaced
+	let imagePath = product.imageUrl;
+	if (data.image != null && data.image.size > 0) {
+		await fs.unlink(`public${product.imageUrl}`);
+		imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+		// Conver file to buffer and pass that to write file
+		await fs.writeFile(
+			`public${imagePath}`,
+			Buffer.from(await data.image.arrayBuffer())
+		);
+	}
+
+	try {
+		const updatedProduct = await db.product.update({
+			where: { id },
+			data: {
+				name: data.name,
+				description: data.description,
+				priceInCents: data.priceInCents,
+				imageUrl: imagePath,
+			},
+		});
+
+		console.log("Product updated successfully:", updatedProduct);
+	} catch (err) {
+		console.error(`Error updating product:`, err);
+	}
+
+	redirect("/admin/products");
+}
+
 export async function toggleProductAvailability(
 	id: string,
 	isAvailableForPurchase: boolean
