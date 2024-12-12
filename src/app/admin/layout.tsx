03f6f785674ee1c835420d23d70f893ff787ea0f
mgz11 <mgonz112000@gmail.com>
2024-12-12 14:47:19 -0800

    added logo to admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import { Nav, NavLink } from "@/components/Nav";
+import Image from "next/image";
 
 /*
 This causes admin pages not to be cached since admin pages
@@ -14,9 +15,23 @@ export default function AdminLayout({
 	return (
 		<>
 			<Nav>
-				<NavLink href="/admin">Dashboard</NavLink>
-				<NavLink href="/admin/products">Products</NavLink>
-				<NavLink href="/admin/orders">Sales</NavLink>
+				<div className="flex-1 flex items-center p-4 relative">
+					<div className="absolute top-1/2 transform -translate-y-1/2 left-4 flex items-center space-x-2">
+						<Image
+							src="/assets/logo.png"
+							alt="logo"
+							width={100} // Restrict width
+							height={100} // Restrict height
+							className="object-contain"
+						/>
+					</div>
+				</div>
+				<div className="flex justify-center">
+					<NavLink href="/admin">Dashboard</NavLink>
+					<NavLink href="/admin/products">Products</NavLink>
+					<NavLink href="/admin/orders">Sales</NavLink>
+				</div>
+				<div className="flex-1"></div>
 			</Nav>
 			<div className="container my-6">{children}</div>
 		</>
